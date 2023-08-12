0cfced6bb88cdf79bf2dcf46dc9238e329f6a3fe
SausageMuffins <rwang9098@gmail.com>
2023-08-12 15:05:31 +0800

    probably correct viterbi

--- part4.py
+++ part4.py
@@ -67,7 +67,7 @@ def estimate_emission_parameters(train_data, states, observations, state_to_idx,
     num_observations = len(observations)
     
     # Initialize counts
-    emission_counts = np.zeros((num_states, num_observations))
+    emission_counts = np.zeros((num_states, num_observations)) # rows are states, columns are observations
     state_counts = np.zeros(num_states)
 
     # Split the training data into sentences
@@ -77,15 +77,18 @@ def estimate_emission_parameters(train_data, states, observations, state_to_idx,
         lines = sentence.strip().split('\n')
         for line in lines:
             observation, state = line.rsplit(' ', 1)
-            if observation not in observation_to_idx:
-                observation = "#UNK#"
             state_idx = state_to_idx[state]
             observation_idx = observation_to_idx[observation]
             emission_counts[state_idx, observation_idx] += 1
             state_counts[state_idx] += 1
 
-    # Apply smoothing and calculate probabilities
-    emission_probabilities = (emission_counts + k) / (state_counts[:, None] + k * num_observations)
+    # Calculate probabilities for known words
+    emission_probabilities = emission_counts / state_counts[:, None]
+    
+    # Calculate probabilities for unknown words
+    unk_idx = observation_to_idx["#UNK#"]
+    for i in range(num_states):
+        emission_probabilities[i, unk_idx] = k / (state_counts[i] + k)
 
     # Set emission probabilities for START and STOP states to 0
     emission_probabilities[state_to_idx["START"], :] = 0
@@ -115,8 +118,7 @@ def estimate_transmission_parameters(train_data, states, state_to_idx):
     sentences = train_data.strip().split('\n\n')
 
     for sentence in sentences:
-        # print(sentence)
-        lines = sentence.strip().split('\n')
+        lines = sentence.strip().split('\n') # separate each line as an element without the \n
         prev_state = "START"
         for line in lines:
             _, current_state = line.rsplit(' ', 1)
@@ -134,8 +136,59 @@ def estimate_transmission_parameters(train_data, states, state_to_idx):
     transmission_probabilities[nan_mask] = 0 # set all transition probabilities from STOP to 0
     return transmission_probabilities
 
+# def viterbi(test_data, states, state_to_idx, observation_to_idx, emission_probabilities, transmission_probabilities):
+#     # Split the test data into sentences
+#     sentences = test_data.strip().split('\n\n')
+#     predicted_tags = []
+    
+#     for sentence in sentences:
+#         words = sentence.strip().split('\n')
+#         num_words = len(words)
+#         num_states = len(states)
+
+#         # Initialize Viterbi matrix
+#         viterbi_matrix = np.zeros((num_states, num_words+2)) # the rows of the viterbi matrix correspond to the states while the columns correspond to the words
+
+#         # Initialization 
+#         start_idx = state_to_idx["START"]
+#         stop_idx = state_to_idx["STOP"]
+#         viterbi_matrix[start_idx, 0] = 1 # base case
+
+#         # Recursion step
+#         for j in range(1, num_words+1):
+#             word = words[j-1]
+#             if word not in observation_to_idx:
+#                 word = "#UNK#"
+#             observation_idx = observation_to_idx[word]
+#             for s in range(num_states): # s is the current state
+                
+#                 if s == start_idx or s == stop_idx: # skip START and STOP states
+#                     continue
+                
+#                 trans_prob = transmission_probabilities[:, s] * viterbi_matrix[:, j-1] # find transition probabilities from all states to current state
+#                 max_trans_prob = np.max(trans_prob) # most likely transition probability
+#                 viterbi_matrix[s, j] = max_trans_prob * emission_probabilities[s, observation_idx]
+                
+#         # Termination step
+#         # calculate transition probability from all states to STOP state
+#         stop_trans_prob = transmission_probabilities[:, stop_idx] * viterbi_matrix[:, num_words] # 0th index is START state
+#         max_stop_trans_prob = np.max(stop_trans_prob)
+#         viterbi_matrix[stop_idx, num_words+1] = max_stop_trans_prob # calculate probability from last word to STOP state
+        
+#         best_last_tag = np.argmax(viterbi_matrix[:, -2]) # find the most likely last tag of the last word
+#         best_path = [best_last_tag]
+
+#         # Backtrack to find the best path
+#         for i in range(num_words, 1, -1):
+#             best_tag = np.argmax(viterbi_matrix[:, i-1])
+#             best_path.insert(0, best_tag)
+        
+#         # Convert state indices to state names
+#         predicted_tags.append([states[s] for s in best_path])
+#     #print(predicted_tags)
+#     return predicted_tags
+
 def viterbi(test_data, states, state_to_idx, observation_to_idx, emission_probabilities, transmission_probabilities):
-    # Split the test data into sentences
     sentences = test_data.strip().split('\n\n')
     predicted_tags = []
     
@@ -144,13 +197,12 @@ def viterbi(test_data, states, state_to_idx, observation_to_idx, emission_probab
         num_words = len(words)
         num_states = len(states)
 
-        # Initialize Viterbi matrix
-        viterbi_matrix = np.zeros((num_states, num_words+2)) # the rows of the viterbi matrix correspond to the states while the columns correspond to the words
+        viterbi_matrix = np.zeros((num_states, num_words+2))
+        backpointers = np.zeros((num_states, num_words+2), dtype=int) # Added backpointers for backtracking
 
-        # Initialization 
         start_idx = state_to_idx["START"]
         stop_idx = state_to_idx["STOP"]
-        viterbi_matrix[start_idx, 0] = 1 # base case
+        viterbi_matrix[start_idx, 0] = 1
 
         # Recursion step
         for j in range(1, num_words+1):
@@ -158,32 +210,32 @@ def viterbi(test_data, states, state_to_idx, observation_to_idx, emission_probab
             if word not in observation_to_idx:
                 word = "#UNK#"
             observation_idx = observation_to_idx[word]
-            for s in range(num_states): # s is the current state
-                
-                if s == start_idx or s == stop_idx: # skip START and STOP states
+            for s in range(num_states):
+                if s == start_idx or s == stop_idx:
                     continue
-                
-                trans_prob = transmission_probabilities[:, s] * viterbi_matrix[:, j-1] # find transition probabilities from all states to current state
-                max_trans_prob = np.max(trans_prob) # most likely transition probability
+                max_trans_prob = 0
+                for prev_s in range(num_states): # Added loop to consider previous states
+                    trans_prob = transmission_probabilities[prev_s, s] * viterbi_matrix[prev_s, j-1]
+                    if trans_prob > max_trans_prob:
+                        max_trans_prob = trans_prob
                 viterbi_matrix[s, j] = max_trans_prob * emission_probabilities[s, observation_idx]
-                
-        # Termination step
-        # calculate transition probability from all states to STOP state
-        stop_trans_prob = transmission_probabilities[:, stop_idx] * viterbi_matrix[:, num_words] # 0th index is START state
+                backpointers[s, j] = np.argmax(transmission_probabilities[:, s] * viterbi_matrix[:, j-1]) # Added backpointer
+
+        stop_trans_prob = transmission_probabilities[:, stop_idx] * viterbi_matrix[:, num_words]
         max_stop_trans_prob = np.max(stop_trans_prob)
-        viterbi_matrix[stop_idx, num_words+1] = max_stop_trans_prob # calculate probability from last word to STOP state
+        viterbi_matrix[stop_idx, num_words+1] = max_stop_trans_prob
         
-        best_last_tag = np.argmax(viterbi_matrix[:, -2]) # find the most likely last tag of the last word
+        best_last_tag = np.argmax(viterbi_matrix[:, -2])
         best_path = [best_last_tag]
 
-        # Backtrack to find the best path
-        for i in range(num_words, 1, -1):
-            best_tag = np.argmax(viterbi_matrix[:, i-1])
+        # Backtrack using backpointers
+        for i in range(num_words, 1, -1): # Modified backtracking using backpointers
+            best_tag = backpointers[best_last_tag, i]
             best_path.insert(0, best_tag)
+            best_last_tag = best_tag
         
-        # Convert state indices to state names
         predicted_tags.append([states[s] for s in best_path])
-    #print(predicted_tags)
+
     return predicted_tags
 
 def write_predictions_to_file(file_path, predicted_sequences, test_data):
