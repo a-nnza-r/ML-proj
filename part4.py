f96cb7a8a07143cd12d8c5d648f6a9c62cddfb55
SausageMuffins <rwang9098@gmail.com>
2023-08-12 18:53:49 +0800

    validation based on laplace smoothing

--- part4.py
+++ part4.py
@@ -1,4 +1,5 @@
 import numpy as np
+import evalResult
 
 def prepare_data(file_path):
     """Prepare the raw data for training
@@ -83,7 +84,7 @@ def estimate_emission_parameters(train_data, states, observations, state_to_idx,
             state_counts[state_idx] += 1
 
     # Calculate probabilities for known words
-    emission_probabilities = emission_counts / state_counts[:, None]
+    emission_probabilities = (emission_counts + k) / (state_counts[:, None] + k * num_observations) # LAPLACE SMOOTHING
     
     # Calculate probabilities for unknown words
     unk_idx = observation_to_idx["#UNK#"]
@@ -135,58 +136,6 @@ def estimate_transmission_parameters(train_data, states, state_to_idx):
     nan_mask = np.isnan(transmission_probabilities)
     transmission_probabilities[nan_mask] = 0 # set all transition probabilities from STOP to 0
     return transmission_probabilities
-
-# def viterbi(test_data, states, state_to_idx, observation_to_idx, emission_probabilities, transmission_probabilities):
-#     # Split the test data into sentences
-#     sentences = test_data.strip().split('\n\n')
-#     predicted_tags = []
-    
-#     for sentence in sentences:
-#         words = sentence.strip().split('\n')
-#         num_words = len(words)
-#         num_states = len(states)
-
-#         # Initialize Viterbi matrix
-#         viterbi_matrix = np.zeros((num_states, num_words+2)) # the rows of the viterbi matrix correspond to the states while the columns correspond to the words
-
-#         # Initialization 
-#         start_idx = state_to_idx["START"]
-#         stop_idx = state_to_idx["STOP"]
-#         viterbi_matrix[start_idx, 0] = 1 # base case
-
-#         # Recursion step
-#         for j in range(1, num_words+1):
-#             word = words[j-1]
-#             if word not in observation_to_idx:
-#                 word = "#UNK#"
-#             observation_idx = observation_to_idx[word]
-#             for s in range(num_states): # s is the current state
-                
-#                 if s == start_idx or s == stop_idx: # skip START and STOP states
-#                     continue
-                
-#                 trans_prob = transmission_probabilities[:, s] * viterbi_matrix[:, j-1] # find transition probabilities from all states to current state
-#                 max_trans_prob = np.max(trans_prob) # most likely transition probability
-#                 viterbi_matrix[s, j] = max_trans_prob * emission_probabilities[s, observation_idx]
-                
-#         # Termination step
-#         # calculate transition probability from all states to STOP state
-#         stop_trans_prob = transmission_probabilities[:, stop_idx] * viterbi_matrix[:, num_words] # 0th index is START state
-#         max_stop_trans_prob = np.max(stop_trans_prob)
-#         viterbi_matrix[stop_idx, num_words+1] = max_stop_trans_prob # calculate probability from last word to STOP state
-        
-#         best_last_tag = np.argmax(viterbi_matrix[:, -2]) # find the most likely last tag of the last word
-#         best_path = [best_last_tag]
-
-#         # Backtrack to find the best path
-#         for i in range(num_words, 1, -1):
-#             best_tag = np.argmax(viterbi_matrix[:, i-1])
-#             best_path.insert(0, best_tag)
-        
-#         # Convert state indices to state names
-#         predicted_tags.append([states[s] for s in best_path])
-#     #print(predicted_tags)
-#     return predicted_tags
 
 def viterbi(test_data, states, state_to_idx, observation_to_idx, emission_probabilities, transmission_probabilities):
     sentences = test_data.strip().split('\n\n')
@@ -260,45 +209,108 @@ def write_predictions_to_file(file_path, predicted_sequences, test_data):
                 file.write(word + ' ' + predicted_sequences[i][j] + '\n')
             file.write('\n')
 
+def compute_scores(gold_file_path, predicted_file_path):
+    ## code adapted from evalResult ##
+    gold = open(gold_file_path, "r", encoding="utf-8")
+    predicted = open(predicted_file_path, "r", encoding="utf-8")
+
+    #column separator
+    separator = ' '
+
+    #the column index for tags
+    outputColumnIndex = 1
+    
+    #Read Gold data
+    observed = evalResult.get_observed(gold, separator, outputColumnIndex)
+
+    #Read Predction data
+    predicted = evalResult.get_predicted(predicted, separator, outputColumnIndex)
+
+    correct_entity, correct_sentiment, entity_prec, entity_rec, entity_f, sentiment_prec, sentiment_rec, sentiment_f = evalResult.compare_observed_to_predicted(observed, predicted)
+    
+    return correct_entity, correct_sentiment, entity_prec, entity_rec, entity_f, sentiment_prec, sentiment_rec, sentiment_f
+
+
 def predict_es():
     file_path = "Data\\ES\\train"
     state_to_idx, observation_to_idx, states, observations, train_data = prepare_data(file_path)
 
-    emission_prob = estimate_emission_parameters(train_data, states, observations, state_to_idx, observation_to_idx, k=1)
-    
+    validation_file_path = "Data\\ES\\dev.in"
+    with open(validation_file_path, 'r', encoding="utf-8") as file:
+        validation_data = file.read()
+        
+    gold_file_path = "Data\\ES\\dev.out"
+        
+    # Define a range of k values to try
+    k_values = [0.1 * i for i in range(1, 11)] # k = 0.1, 0.2, ..., 1.0
+
     transition_prob = estimate_transmission_parameters(train_data, states, state_to_idx)
-    
-    test_file_path = "Data\\ES\\dev.in"
-    with open(test_file_path, 'r', encoding="utf-8") as file:
-        test_data = file.read()
-    
-    # print(state_to_idx)
-    # print(transition_prob)
-    
-    predicted_tags = viterbi(test_data, states, state_to_idx, observation_to_idx, emission_prob, transition_prob)
-    # print(predicted_tags)
-    
-    write_predictions_to_file("Testing\\es.dev.test", predicted_tags, test_data)
+
+    # Iterate through each k value
+    for k in k_values:
+        # Estimate emission probabilities with the current k
+        emission_prob = estimate_emission_parameters(train_data, states, observations, state_to_idx, observation_to_idx, k=k)
+        
+        # Run Viterbi on the validation data
+        predicted_tags = viterbi(validation_data, states, state_to_idx, observation_to_idx, emission_prob, transition_prob)
+        output_file_path = "Testing\\es.dev.k" + str(k)
+        write_predictions_to_file(output_file_path, predicted_tags, validation_data)
+        
+        correct_entity, correct_sentiment, entity_prec, entity_rec, entity_f, sentiment_prec, sentiment_rec, sentiment_f = compute_scores(gold_file_path, output_file_path)
+        
+        
+        print("k = " + str(k))
+        print("correct_entity = " + str(correct_entity))
+        print("correct_sentiment = " + str(correct_sentiment))
+        print("entity_prec = " + str(entity_prec))
+        print("entity_rec = " + str(entity_rec))
+        print("entity_f = " + str(entity_f))
+        print("sentiment_prec = " + str(sentiment_prec))
+        print("sentiment_rec = " + str(sentiment_rec))
+        print("sentiment_f = " + str(sentiment_f))
+        print()
+        
+
     
 def predict_ru():
     file_path = "Data\\RU\\train"
     state_to_idx, observation_to_idx, states, observations, train_data = prepare_data(file_path)
 
-    emission_prob = estimate_emission_parameters(train_data, states, observations, state_to_idx, observation_to_idx, k=1)
-    
+    validation_file_path = "Data\\RU\\dev.in"
+    with open(validation_file_path, 'r', encoding="utf-8") as file:
+        validation_data = file.read()
+        
+    gold_file_path = "Data\\RU\\dev.out"
+        
+    # Define a range of k values to try
+    k_values = [0.1 * i for i in range(1, 11)] # k = 0.1, 0.2, ..., 1.0
+
     transition_prob = estimate_transmission_parameters(train_data, states, state_to_idx)
-    
-    test_file_path = "Data\\RU\\dev.in"
-    with open(test_file_path, 'r', encoding="utf-8") as file:
-        test_data = file.read()
-    
-    # print(state_to_idx)
-    # print(transition_prob)
-    
-    predicted_tags = viterbi(test_data, states, state_to_idx, observation_to_idx, emission_prob, transition_prob)
-    # print(predicted_tags)
-    
-    write_predictions_to_file("Testing\\ru.dev.test", predicted_tags, test_data)
+
+    # Iterate through each k value
+    for k in k_values:
+        # Estimate emission probabilities with the current k
+        emission_prob = estimate_emission_parameters(train_data, states, observations, state_to_idx, observation_to_idx, k=k)
+        
+        # Run Viterbi on the validation data
+        predicted_tags = viterbi(validation_data, states, state_to_idx, observation_to_idx, emission_prob, transition_prob)
+        output_file_path = "Testing\\ru.dev.k" + str(k)
+        write_predictions_to_file(output_file_path, predicted_tags, validation_data)
+        
+        correct_entity, correct_sentiment, entity_prec, entity_rec, entity_f, sentiment_prec, sentiment_rec, sentiment_f = compute_scores(gold_file_path, output_file_path)
+        
+        
+        print("k = " + str(k))
+        print("correct_entity = " + str(correct_entity))
+        print("correct_sentiment = " + str(correct_sentiment))
+        print("entity_prec = " + str(entity_prec))
+        print("entity_rec = " + str(entity_rec))
+        print("entity_f = " + str(entity_f))
+        print("sentiment_prec = " + str(sentiment_prec))
+        print("sentiment_rec = " + str(sentiment_rec))
+        print("sentiment_f = " + str(sentiment_f))
+        print()
+        
     
 if __name__ == "__main__":
     predict_es()
